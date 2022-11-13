ae61581935c83fbc1b50e3de4d9cbbf9501a6216
cycgitub <117264784+cycgitub@users.noreply.github.com>
2022-11-13 14:10:43 +0800

    Add files via upload

---
+++ 选择结构练习.py
@@ -0,0 +1,17 @@
+#认识循环
+for x in range(1,100):
+    print("hello world") #循环操作：重复执行的业务
+
+
+#重复登录
+#匹配1000个学号，匹配某个特定学号
+
+#for循环
+
+# for 变量名 in 序列：
+#循环语句
+
+#执行次数=序列中元素的个数
+for s in range(1,11):
+    print(s)
+    print('hello world!')
